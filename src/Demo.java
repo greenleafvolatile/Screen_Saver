56a6288917e1c426c6f5aa0a54c5fccd12199179
Daan Pol <daanpol@tutamail.com>
2019-09-02 21:05:37 +0200

    Added Demo

--- src/Demo.java
+++ src/Demo.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Demo {
+
+    public static void main(String[] args){
+        Screen_Saver_Version_1_0_2_Frame frame=new Screen_Saver_Version_1_0_2_Frame();
+        frame.createAndShowGUI();
+    }
 }
