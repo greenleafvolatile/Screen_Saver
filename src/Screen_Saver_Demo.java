f425eb896bf0e4a6bb0b3d76692e5a81650c67f8
Daan Pol <daanpol@tutamail.com>
2019-09-03 15:26:33 +0200

    Added Screen_Saver_Demo

--- src/Screen_Saver_Demo.java
+++ src/Screen_Saver_Demo.java
@@ -1,4 +1,4 @@
-public class Demo {
+public class Screen_Saver_Demo {
 
     public static void main(String[] args){
         Screen_Saver_Version_1_0_2_Frame frame=new Screen_Saver_Version_1_0_2_Frame();
